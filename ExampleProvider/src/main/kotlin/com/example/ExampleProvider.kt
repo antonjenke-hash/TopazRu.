17a6eef51ca092181b08d0e906bb09c8a63eed1e
antonjenke-hash <antonjenke@gmail.com>
2026-08-27 23:45:53 +0200

    Configure TopasNew24 provider

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -4,18 +4,19 @@ import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.TvType
 
-class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
+class ExampleProvider : MainAPI() {
+
+    override var mainUrl = "https://topasnew24.ru/"
+
+    override var name = "TopasNew24"
+
     override val supportedTypes = setOf(TvType.Movie)
 
-    override var lang = "en"
+    override var lang = "ru"
 
-    // Enable this when your provider has a main page
-    override val hasMainPage = true
+    override val hasMainPage = false
 
-    // This function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
-        return listOf()
+        return emptyList()
     }
 }
